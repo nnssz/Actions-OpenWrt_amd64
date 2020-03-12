6ed950fd4d945dbde51bb17c863ade2dbf166701
nnssz <nonightshift@gmail.com>
2020-03-12 19:42:33 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,10 +6,10 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.4/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Modify the version number
-sed -i 's/OpenWrt/P3TERX build $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/OpenWrt/JonNewton build $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
@@ -23,7 +23,7 @@ cd ..
 
 # Add kernel build user
 [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
-    echo 'CONFIG_KERNEL_BUILD_USER="P3TERX"' >>.config ||
+    echo 'CONFIG_KERNEL_BUILD_USER="JonNewton"' >>.config ||
     sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"P3TERX"@' .config
 
 # Add kernel build domain
